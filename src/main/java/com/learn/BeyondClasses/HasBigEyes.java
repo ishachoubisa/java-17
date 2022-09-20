4028f6c5a8f44b14f6a49b123be221d0028ef2f1
isha choubisa <isha91choubisa1234@gmail.com>
2022-09-20 11:40:03 +0200

    Concept of extending interface

--- src/main/java/com/learn/BeyondClasses/HasBigEyes.java
+++ src/main/java/com/learn/BeyondClasses/HasBigEyes.java
@@ -1,4 +1,5 @@
 package com.learn.BeyondClasses;
 
-public interface HasBigEyes extends Nocturnal,CanFly{
+public interface HasBigEyes extends Nocturnal,CanFly{//Extending two interface is permitted because interface is not instantiated
+    //and unlike abstract classes they do not have constructors and are not of instance initialization.
 }
