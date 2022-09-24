2bb4cf0016915d7947e6dfd15aead828b2790f13
isha choubisa <isha91choubisa1234@gmail.com>
2022-09-24 18:18:40 +0200

    Concept of inbuild Function<T,R> interface.

---
+++ src/main/java/com/learn/lambda/RQ12.java
@@ -0,0 +1,12 @@
+package com.learn.lambda;
+
+import java.util.function.Function;
+
+public class RQ12 {
+    public static void main(String[] args) {
+        Function<Integer,Integer> s = a -> a+4;
+        Function<Integer,Integer> t = a -> a*3;
+        Function<Integer,Integer> c = s.compose(t);
+        System.out.println(c.apply(1));
+    }
+}
