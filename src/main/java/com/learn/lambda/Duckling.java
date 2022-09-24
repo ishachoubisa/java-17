69a1ef8681d22fc3dc99e3e7157622c362b60503
isha choubisa <isha91choubisa1234@gmail.com>
2022-09-24 18:07:34 +0200

    Concept of lambda and method reference

--- src/main/java/com/learn/lambda/Duckling.java
+++ src/main/java/com/learn/lambda/Duckling.java
@@ -2,7 +2,8 @@ package com.learn.lambda;
 
 public class Duckling {
     public static void makeSound(String sound){
-        LearnToSpeak learner = s -> System.out.println(s);
+        LearnToSpeak learner = s -> System.out.println(s);//lambda
+       //LearnToSpeak learner = System.out::println;//method reference
         DuckHelper.teacher(sound,learner);
     }
 }
