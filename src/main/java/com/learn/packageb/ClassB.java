4b42f71c893f9d514c1e0f2eaade60f102e1dd12
isha choubisa <isha91choubisa1234@gmail.com>
2022-10-04 09:59:17 +0200

    concept of how to compile and run from cmd

--- src/main/java/com/learn/packageb/ClassB.java
+++ src/main/java/com/learn/packageb/ClassB.java
@@ -2,10 +2,15 @@ package com.learn.packageb;
 
 import com.learn.packagea.ClassA;
 
+//Compiling to same Directory
 //to create a class in notepad in windows make folders in c drive like this c:\temp\packageb\ClassB.java
 //to create a class in notepad in Mac/Linux make folders in c drive like this /tmp/packageb/ClassB.java
 //to compile this class from cmd line in windows use javac packageb/ClassB.java
 //to run use java packageb.ClassB
+
+//Compiling to Another Directory
+//command to compile:- javac -d classes packageb/ClassB.java
+//command to run:- java -cp classes packageb.ClassB//it will compile it has main method.
 
 public class ClassB {
     public static void main(String[] args) {
