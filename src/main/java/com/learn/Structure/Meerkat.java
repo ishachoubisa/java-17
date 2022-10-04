4946aa0b2911e5d8b66fb64385d862276cd34dfd
isha choubisa <isha91choubisa1234@gmail.com>
2022-10-04 11:05:45 +0200

    concept of ordering elements in class.

---
+++ src/main/java/com/learn/Structure/Meerkat.java
@@ -0,0 +1,11 @@
+package com.learn.Structure;//Must be always the First statement in a class. If present(optional).
+
+import java.util.*;//Must be the Second statement in a class, if present
+
+public class Meerkat {//public class mendetory keyword, must be the third statement in a class
+    double weight;//fields and methods can go in either order
+    public double getWeight(){
+        return weight;
+    }
+    double height;//another field - they don't need to be together.
+}
