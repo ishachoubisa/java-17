61022aa066ba9d291f77f807b8fe4673227079eb
isha choubisa <isha91choubisa1234@gmail.com>
2022-10-05 09:45:19 +0200

    concept of reading and writing member fields

---
+++ src/main/java/com/learn/BuildingBlocks/Name.java
@@ -0,0 +1,7 @@
+package com.learn.BuildingBlocks;
+
+public class Name {
+   String firstname = "Theodore";//write to field
+   String lastname = "Mouse";//write to field
+   String full = firstname + lastname;//read the fields firstname and lastname, write the field full
+}
