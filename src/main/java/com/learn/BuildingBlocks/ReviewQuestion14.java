d43f5119f9a377ef35c185c46f91d51d3d72c75a
ishachoubisa <isha91choubisa1234@gmail.com>
2022-10-17 09:10:22 +0200

    concept of line generating compiler error primitives donot have predefine methods

---
+++ src/main/java/com/learn/BuildingBlocks/ReviewQuestion14.java
@@ -0,0 +1,10 @@
+package com.learn.BuildingBlocks;
+
+public class ReviewQuestion14 {
+    //short numPets = 5L;
+    //int numGrains = 0.2;
+    String name = "Fluffy";
+    //int a = numPets.length();
+    //int b = numGrains.length();
+    int c = name.length();
+}
