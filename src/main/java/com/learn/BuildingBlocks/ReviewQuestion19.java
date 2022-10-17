c93395d8db887c3d2e3b4bbea81a67cafbd427e6
ishachoubisa <isha91choubisa1234@gmail.com>
2022-10-17 10:41:39 +0200

    concept of num1 is long primitive and num2 is Long reference object

---
+++ src/main/java/com/learn/BuildingBlocks/ReviewQuestion19.java
@@ -0,0 +1,10 @@
+package com.learn.BuildingBlocks;
+
+public class ReviewQuestion19 {
+    public static void main(String[] args) {
+        var num1 = Long.parseLong("100");
+        var num2 = Long.valueOf("100");
+        System.out.println(Long.max(num1,num2
+        ));
+    }
+}
