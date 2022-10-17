869ca50766b88546bae67f34b3a42e7a7b3b1bed
ishachoubisa <isha91choubisa1234@gmail.com>
2022-10-17 10:51:52 +0200

    concept of correct int and double value

---
+++ src/main/java/com/learn/BuildingBlocks/Price.java
@@ -0,0 +1,15 @@
+package com.learn.BuildingBlocks;
+
+public class Price {
+    public void admission(){
+        //int Amount = 0b11;
+        //int amount = 9L;
+        //int amount = 0xE;
+        //int amount = 1_2.0;
+        //int amount = 1_0_.0;
+        //int amount = 0b101;
+        double amount = 9_2.1_2;
+        //double amount = 1_2_.0_0;
+        System.out.println(amount);
+    }
+}
