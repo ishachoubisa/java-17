12b9ae6f70a20887ac451846efb178810edfd17b
ishachoubisa <isha91choubisa1234@gmail.com>
2022-10-18 09:27:15 +0200

    concept of text block

---
+++ src/main/java/com/learn/BuildingBlocks/ReviewQuestion16.java
@@ -0,0 +1,11 @@
+package com.learn.BuildingBlocks;
+
+public class ReviewQuestion16 {
+    public static void main(String[] args) {
+        var blocky = """
+                Squirrel \s
+                pigeon   \
+                termite""";
+        System.out.println(blocky);
+    }
+}
