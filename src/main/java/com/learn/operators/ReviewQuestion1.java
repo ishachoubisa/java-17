ca6efe3ca0da0010130365ee5f74ed33fda18c1d
ishachoubisa <isha91choubisa1234@gmail.com>
2022-10-18 16:53:56 +0200

    concept of operators used with boolean

--- src/main/java/com/learn/operators/ReviewQuestion1.java
+++ src/main/java/com/learn/operators/ReviewQuestion1.java
@@ -3,10 +3,14 @@ package com.learn.operators;
 public class ReviewQuestion1 {
     boolean b = true;
     boolean c = !b ;
-    boolean d = (boolean) b;
+    boolean d = c == b;//compile
+    //boolean d = c == !b//compile
+    //boolean d = (boolean) b;//compile
+    //boolean e = ~b;//does not compile,~ can not use with boolean
 
     public static void main(String[] args) {
         ReviewQuestion1 r = new ReviewQuestion1();
         System.out.println(r.c);
+        System.out.println(r.d);
     }
 }
