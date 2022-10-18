2761cabcd36263b1fd6775623050abd0cee711a0
ishachoubisa <isha91choubisa1234@gmail.com>
2022-10-18 09:22:24 +0200

    concept of text block

---
+++ src/main/java/com/learn/BuildingBlocks/KitchenSink.java
@@ -0,0 +1,13 @@
+package com.learn.BuildingBlocks;
+
+public class KitchenSink {
+    private int numForks;
+
+    public static void main(String[] args) {
+        int numKnives;
+        System.out.println("""
+                "# forks = " + numForks + 
+                " # knives = " + numKnives + 
+                # cups = 0""");
+    }
+}
