22d025e1e45fb9f603774f086af817d9ac11ebd5
ishachoubisa <isha91choubisa1234@gmail.com>
2022-10-19 09:37:30 +0200

    concept of type casting

---
+++ src/main/java/com/learn/operators/CandyCounter.java
@@ -0,0 +1,13 @@
+package com.learn.operators;
+
+public class CandyCounter {
+//    static long addCandy(double fruit, float vegetables){
+//        return (int)fruit+vegetables;//Does not compile.Required long provided float.
+//    }
+
+    public static void main(String[] args) {
+//        System.out.println(addCandy(1.4,2.4f)+",");
+//        System.out.println(addCandy(1.9,(float)4)+",");
+//        System.out.println(addCandy((long)(int)(short)2,(float)4));
+    }
+}
