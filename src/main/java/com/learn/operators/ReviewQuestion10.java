37d014fcde349ed3346803d9ae2bc190b1900e15
ishachoubisa <isha91choubisa1234@gmail.com>
2022-10-19 10:18:16 +0200

    concept of type casting

---
+++ src/main/java/com/learn/operators/ReviewQuestion10.java
@@ -0,0 +1,12 @@
+package com.learn.operators;
+
+public class ReviewQuestion10 {
+    public static void main(String[] args) {
+        short height=1,weight=3;
+        //short zebra = (byte)weight*(byte)height;//DOES NOT COMPILE. Required short provided int
+        double ox = 1+height*2+weight;
+        long girrafe =1+9%height+1;
+        //System.out.println(zebra+","+ox+","+girrafe);
+
+    }
+}
