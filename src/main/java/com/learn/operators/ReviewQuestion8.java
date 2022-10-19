940e3b9d3d5c3c84306d452b4acf88e107c40ad3
ishachoubisa <isha91choubisa1234@gmail.com>
2022-10-19 10:04:04 +0200

    concept of compound assignment operator

---
+++ src/main/java/com/learn/operators/ReviewQuestion8.java
@@ -0,0 +1,11 @@
+package com.learn.operators;
+
+public class ReviewQuestion8 {
+    public static void main(String[] args) {
+        int pig = (short)4;
+        pig =pig++;
+        long goat = (int)2;
+        goat-=1.0;
+        System.out.println(pig+","+goat);
+    }
+}
