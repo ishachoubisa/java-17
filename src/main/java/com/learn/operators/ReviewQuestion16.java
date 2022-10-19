9ddd47d381c5fa6b8d748c7df7283ba37686aec4
ishachoubisa <isha91choubisa1234@gmail.com>
2022-10-19 10:45:06 +0200

    concept of type casting

---
+++ src/main/java/com/learn/operators/ReviewQuestion16.java
@@ -0,0 +1,9 @@
+package com.learn.operators;
+
+public class ReviewQuestion16 {
+   // int note = 1*2 =(long)3;//DOES NOT COMPILE.Required int provided long
+    //short melody =(byte)(double)(note*=2);
+    //double song = melody;
+    //float symphony = (float) ((song==1_000f)?song*2L:song);
+
+}
