00c0d9924063279ed954eed7f54f8e19ecd78649
ishachoubisa <isha91choubisa1234@gmail.com>
2022-10-19 09:36:51 +0200

    concept of exclusive OR

---
+++ src/main/java/com/learn/operators/ReviewQuestion4.java
@@ -0,0 +1,10 @@
+package com.learn.operators;
+
+public class ReviewQuestion4 {
+    public static void main(String[] args) {
+        boolean canine = true,wolf = true;
+        int teeth = 20;
+        canine = (teeth != 10)^(wolf = false);
+        System.out.println(canine+","+teeth+","+wolf);
+    }
+}
