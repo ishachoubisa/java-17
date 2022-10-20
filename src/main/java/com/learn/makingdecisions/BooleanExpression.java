c2d1e564da388ad7a41013633c415f2a18510d40
ishachoubisa <isha91choubisa1234@gmail.com>
2022-10-20 13:11:16 +0200

    concept of boolean expression

---
+++ src/main/java/com/learn/makingdecisions/BooleanExpression.java
@@ -0,0 +1,11 @@
+package com.learn.makingdecisions;
+
+public class BooleanExpression {
+    public static void main(String[] args) {
+        int hourOfDay=1;
+//        if(hourOfDay){ //DOES NOT COMPILE.This statement may be valid in some other programming and scripting languages,but
+                         //not in Java,where 0 and 1 are not considered boolean values.
+//
+//        }
+    }
+}
