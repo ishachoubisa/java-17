ac137a4099e926caa2fe1a9a0164df0bc4916546
ishachoubisa <isha91choubisa1234@gmail.com>
2022-11-03 09:49:51 +0100

    concept of if and else statement

--- src/main/java/com/learn/makingdecisions/ReviewQuestion2.java
+++ src/main/java/com/learn/makingdecisions/ReviewQuestion2.java
@@ -6,7 +6,7 @@ public class ReviewQuestion2 {
     long humidity = -temperature +temperature*3;
     if(temperature >=4)
         if(humidity <6)System.out.println("Too Low");
-        else System.out.println("Just Right");
-        else System.out.println("Too High");
+        else System.out.println("Just Right");//this else is optional if do not put it then also the code will run
+        else System.out.println("Too High");//this else also optional
     }
 }
