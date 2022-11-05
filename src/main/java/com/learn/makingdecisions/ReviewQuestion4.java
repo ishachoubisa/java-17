d6c2ef0e54e7aed543813a9daa6569168e2bfd20
ishachoubisa <isha91choubisa1234@gmail.com>
2022-11-05 13:31:06 +0100

    concept of switch expression

---
+++ src/main/java/com/learn/makingdecisions/ReviewQuestion4.java
@@ -0,0 +1,19 @@
+package com.learn.makingdecisions;
+
+public class ReviewQuestion4 {
+//    void printReptile(int category){
+////         var type = switch (category){//does not compile, switch expression requires all possible case values to be handled.
+                                          //If default statement was added,code would compile and print turtle.
+////             case 1,2 -> "Snake";
+////             case 3,4 -> "Lizard";
+////             case 5,6 -> "Turtle";
+////             case 7,8 -> "Alligator";
+////         };
+////         System.out.println(type);
+////     }
+//
+//    public static void main(String[] args) {
+//         ReviewQuestion4 rq = new ReviewQuestion4();
+//         rq.printReptile(6);
+//    }
+}
