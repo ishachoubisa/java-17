5a898d90b0aa9c57db4c3c53f5b729fd42bc0408
ishachoubisa <isha91choubisa1234@gmail.com>
2022-11-05 10:58:35 +0100

    concept of switch expression

---
+++ src/main/java/com/learn/makingdecisions/ReviewQuestion15.java
@@ -0,0 +1,19 @@
+package com.learn.makingdecisions;
+
+public class ReviewQuestion15 {
+    public static void main(String[] args) {
+       final char a = 'A', e = 'E';
+       char grade = 'B';
+       switch (grade){
+           default:
+           case a:
+           //case 'B':'C':System.out.println("great");//Does not compile,because instead of colon comma should be use to separate
+               //combined case statements.
+           case 'D':
+               System.out.println("good");break;
+           case e:
+           case 'F':
+               System.out.println("not good");
+       }
+    }
+}
