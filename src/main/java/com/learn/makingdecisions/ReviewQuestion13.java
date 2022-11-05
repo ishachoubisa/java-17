ea132d1fdd9372d2dbb6373fdf96d1eee37ab433
ishachoubisa <isha91choubisa1234@gmail.com>
2022-11-05 10:41:34 +0100

    concept of do while loop

---
+++ src/main/java/com/learn/makingdecisions/ReviewQuestion13.java
@@ -0,0 +1,14 @@
+package com.learn.makingdecisions;
+
+public class ReviewQuestion13 {
+    public static void main(String[] args) {
+        boolean keepGoing = true;
+        int result =15,meters=10;
+        do{
+            meters--;
+            if(meters==8) keepGoing = false;
+                    result -= 2;
+        }while(keepGoing);
+        System.out.println(result);
+    }
+}
