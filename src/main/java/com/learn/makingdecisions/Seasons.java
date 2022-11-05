3e5d28ca7142c157717033297d7360e0522e3828
ishachoubisa <isha91choubisa1234@gmail.com>
2022-11-05 10:10:17 +0100

    concept of switch expression and muiltiple values of case in ,separated by commas

---
+++ src/main/java/com/learn/makingdecisions/Seasons.java
@@ -0,0 +1,17 @@
+package com.learn.makingdecisions;
+
+public class Seasons {
+    public void printSeason(int month){
+        switch (month){
+            case 1,2,3 -> System.out.println("winter");
+            case 4,5,6 -> System.out.println("spring");
+            case 7,8,9 -> System.out.println("summer");
+            case 10,11,12 -> System.out.println("fall");
+        }
+    }
+
+    public static void main(String[] args) {
+        Seasons s = new Seasons();
+        s.printSeason(6);
+    }
+}
