5809c508c780eaadbab3ae15ba00d2ddcfb47a0e
ishachoubisa <isha91choubisa1234@gmail.com>
2022-11-05 14:14:59 +0100

    concept of label break and label continue in for loop

---
+++ src/main/java/com/learn/makingdecisions/ReviewQuestion9.java
@@ -0,0 +1,16 @@
+package com.learn.makingdecisions;
+
+public class ReviewQuestion9 {
+    public static void main(String[] args) {
+        int count = 0;
+        BUNNY: for(int row=1; row<=3; row++)
+            RABBIT: for(int col=0; col<3; col++){
+                System.out.println(row+","+col);
+            if((col+row)%2==0)
+                continue;
+            count++;
+                //System.out.println(count);
+            }
+        System.out.println(count);
+    }
+}
