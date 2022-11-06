a3ec94bfc41aab9866f0e735f9de683fc36f9a26
ishachoubisa <isha91choubisa1234@gmail.com>
2022-11-06 09:19:25 +0100

    concept of switch statement

---
+++ src/main/java/com/learn/makingdecisions/ReviewQuestion21.java
@@ -0,0 +1,18 @@
+package com.learn.makingdecisions;
+
+public class ReviewQuestion21 {
+//     static  void printZooKeeper(long id){
+//            System.out.println(switch(id){
+//                case 10 -> {"lisa"}
+//                case 20 -> {yield "alley";};
+//                case 30 -> "geby";
+//                case 30 -> "sahar";
+//                default ->"undefined";
+//            });
+//        }
+
+    public static void main(String[] args) {
+        //printZooKeeper(20);
+    }
+}
+
