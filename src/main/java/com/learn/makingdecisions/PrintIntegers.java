dc0e0c617eb47de33b068b1fccc754b1a1551a76
ishachoubisa <isha91choubisa1234@gmail.com>
2022-11-06 11:27:52 +0100

    concept of do while loop

---
+++ src/main/java/com/learn/makingdecisions/PrintIntegers.java
@@ -0,0 +1,9 @@
+package com.learn.makingdecisions;
+
+public class PrintIntegers {
+    public static void main(String[] args) {
+        int y = -2;
+        do System.out.println(++y + "");
+        while(y<=5);
+    }
+}
