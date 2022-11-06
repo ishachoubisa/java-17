546ca31f8e2323adf05a53539cf4ac9661e53ec5
ishachoubisa <isha91choubisa1234@gmail.com>
2022-11-06 11:20:01 +0100

    concept of for each loop

---
+++ src/main/java/com/learn/makingdecisions/ReviewQuestion24.java
@@ -0,0 +1,11 @@
+package com.learn.makingdecisions;
+
+public class ReviewQuestion24 {
+    public static void main(String[] args) {
+//        for(var friend in friends)//DOES NOT COMPILE.in used inside is not valid syntax of for each loop,instead colon used here
+                                  //will accept set,int[],collections at place of friends.
+//        {
+//            System.out.println(friend);
+//        }
+    }
+}
