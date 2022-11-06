2cfaed20c9e6bf3dc17ecf4888ee193cdb4b19af
ishachoubisa <isha91choubisa1234@gmail.com>
2022-11-06 09:27:15 +0100

    concept of if and else statement

---
+++ src/main/java/com/learn/makingdecisions/ReviewQuestion23.java
@@ -0,0 +1,14 @@
+package com.learn.makingdecisions;
+
+public class ReviewQuestion23 {
+    public static void main(String[] args) {
+        int penguin = 50,turtle = 75;
+        boolean older = penguin>=turtle;
+        if(older=true)
+            System.out.println("success");
+        else
+            System.out.println("failure");
+//        else if(penguin!=50)//Does not compile. where is if statement.
+//            System.out.println("Others");
+    }
+}
