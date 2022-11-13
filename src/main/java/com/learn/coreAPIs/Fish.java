79b6ce4655e1e212e60c015b580dbf8f74b5196f
ishachoubisa <isha91choubisa1234@gmail.com>
2022-11-13 03:51:46 +0100

    concept of String data type

---
+++ src/main/java/com/learn/coreAPIs/Fish.java
@@ -0,0 +1,11 @@
+package com.learn.coreAPIs;
+
+public class Fish {
+    public static void main(String[] args) {
+        int numFish = 4;
+        String type = "tuna";
+        //String anotherFish = numFish + 1;//Does not compile.Required String provided int.
+        //System.out.println(anotherFish + " " + type);
+        System.out.println(numFish + " " + 1);
+    }
+}
