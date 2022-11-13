b00d42c405d1c68073510bff029ff8b6a721bbb1
ishachoubisa <isha91choubisa1234@gmail.com>
2022-11-13 04:06:41 +0100

    concept of array declaration and initialization

---
+++ src/main/java/com/learn/coreAPIs/ReviewQuestion2.java
@@ -0,0 +1,12 @@
+package com.learn.coreAPIs;
+
+public class ReviewQuestion2 {
+    public static void main(String[] args) {
+        int[][] scores = new int[5][];
+        Object[][][] cubies = new Object[3][0][5];
+        //String beans[] = new beans[];//Does not compile.In declaration we use data type not variable name after new.
+        java.util.Date[] dates[] = new java.util.Date[2][];
+        //int[][] types = new int[];//Does not compile.Declare twoD array and after new oneD array.
+        //int[][] java = new int[][];//Does not compile. Array initializer expected.
+    }
+}
