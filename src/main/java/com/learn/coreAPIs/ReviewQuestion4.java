c3436ed8cb032fe09a99409f915c17b27d441f78
ishachoubisa <isha91choubisa1234@gmail.com>
2022-11-13 04:35:20 +0100

    concept of StringBuilder

---
+++ src/main/java/com/learn/coreAPIs/ReviewQuestion4.java
@@ -0,0 +1,9 @@
+package com.learn.coreAPIs;
+
+public class ReviewQuestion4 {
+    public static void main(String[] args) {
+        var sb = new StringBuilder();
+        sb.append("aaa").insert(1,"bb").insert(4,"ccc");
+        System.out.println(sb);
+    }
+}
