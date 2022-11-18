ff3e600e77871b7ed20676725d2722d2463ad68f
ishachoubisa <isha91choubisa1234@gmail.com>
2022-11-18 13:39:08 +0100

    concept of access modifier

---
+++ src/main/java/com/learn/my/city/School.java
@@ -0,0 +1,15 @@
+package com.learn.my.city;
+
+import com.learn.my.school.*;
+
+public class School {
+    public static void main(String[] args) {
+        //Classroom room = new Classroom();
+        //Classroom room1 = new Classroom(101,"kamilia");//DOES NOT COMPILE.It is package access in Classroom class can not
+                                                         //access outside package.
+        //System.out.println(room.floor);
+        //System.out.println(room.roomNumber);//DOES NOT COMPILE.It is private in Classroom class
+        //System.out.println(room.teacherName);//DOES NOT COMPILE.It is protected in Classroom class can not be access outside
+                                               //package
+    }
+}
