616533ef76ac3d686294404d4a0fe296d3ab2896
ishachoubisa <isha91choubisa1234@gmail.com>
2022-11-20 12:08:11 +0100

    concept of public modifier,import static member and field

---
+++ src/main/java/com/learn/Methods/Chimp.java
@@ -0,0 +1,12 @@
+package com.learn.Methods;
+
+import com.learn.rope.*;
+import static com.learn.rope.Rope.*;
+
+public class Chimp {
+    public static void main(String[] args) {
+        Rope.swing();
+        new Rope().swing();
+        System.out.println(LENGTH);
+    }
+}
