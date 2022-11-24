cfac915483d78b9b561945de66a88edb977d94a5
ishachoubisa <isha91choubisa1234@gmail.com>
2022-11-24 15:54:24 +0100

    concept of calling one constructor from another constructor

--- src/main/java/com/learn/classdesign/Hamster.java
+++ src/main/java/com/learn/classdesign/Hamster.java
@@ -2,11 +2,13 @@ package com.learn.classdesign;
 
 public class Hamster {
     private String color;
-    private int weight;
+    private int weight=5;
 
     public Hamster(int weight, String color) {//First constructor
         this.weight = weight;
         this.color = color;
+        System.out.println(weight);
+        System.out.println(color);
     }
 
     //Calling first constructor in second constructor
@@ -14,8 +16,13 @@ public class Hamster {
         this(weight,"brown");//Now Java calls the constructor that takes two parameter,with weight and color
                                    //set as expected
 
-        this.weight = weight;
+        System.out.println(weight);
+        System.out.println(color);
+        this.weight=weight;
+        //weight = this.weight;//it will not change the value of parameter.
         color = "blue";
+        System.out.println(weight);
+        System.out.println(color);
 
         //Hamster(weight,color){}//DOES NOT COMPILE.Constructor does not call by calling them like normal method.
         //Constructors can be called only by writing new before the name of the constructor.
@@ -25,6 +32,9 @@ public class Hamster {
         //it creates two new objects one with default weight and color values, other with desire weight and color values.
         //One is discarded after it is created.
 
+    }
 
+    public static void main(String[] args) {
+        new Hamster(4);
     }
 }
